ff202ff5f1d7304f14e59c7b9c798950d515abd3
“mgoel1” <mgoel@walmartlabs.com>
2016-03-26 15:48:28 -0700

    initial implementation of search bar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,3 +1,44 @@
-/**
- * Created by mgoel1 on 3/24/16.
- */
+
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // set the initial state
+    this.state = {
+      term: ''
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          placeholder="Search"
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
+
+/*
+
+Some notes for above:
+
+  Handling events in React has two steps:
+     1. Event handler: function that runs when event occurs
+     2. Pass event handler to element we want to monitor
+
+  State is a plain JS object used to record and react to user events
+  State causes component and child components to re-render
+
+  Functional components do not have state, only class based components do
+
+  Input is a controlled component and its value is controlled by the component state
+
+*/
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar'
+
+const YOUTUBE_API_KEY = "AIzaSyBndGu5Hg19lw0YIT13sOz3Vu-j5M0X-lM";
+
 // Create a new component. This component should produce
 // some HTML
 
 // => is referred to fat arrow
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+    <SearchBar />
+  </div>
+  );
 }
 
 // Take this component's generated HTML and put it
